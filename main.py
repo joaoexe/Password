f7ce3088128bd6b65edd61291dfb7806cd2bb964
João <joao.0807mh@gmail.com>
2024-09-05 19:08:42 -0300

    first lal

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%¨&*(){}[]<>:,."
+
+while True:
+    try:
+        len = int(input("[+] - PASSWORD LENGTH: "))
+        quantity = int(input("[+] = AMOUNT OF PASSWORD: "))
+    except ValueError:
+        print("Invalid Entry!!!!")
+    else:
+        for x in range(0, quantity):
+            passwd = ""
+            for x in range(0, len):
+                passchar = random.choice(char)
+                passwd = passwd + passchar
+            print("[+] - Generated Password: ", passwd)
+        break
